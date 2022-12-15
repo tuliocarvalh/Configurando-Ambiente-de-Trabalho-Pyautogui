a46d51d049a78a943c5e38277b781606b17b5970
tuliocarvalh <tuliocarvalh@gmail.com>
2022-12-15 13:34:31 -0300

    First codes

---
+++ amb.py
@@ -0,0 +1,17 @@
+import pyautogui as pg 
+import os
+from datetime import datetime
+
+
+def ambiente_work():
+    os.system("start Chrome.exe")
+    pg.hotkey('win', 'up')
+    
+def sleep():
+    
+
+if hora == 8 :
+    ambiente_work()
+
+if hora == 23 :
+    sleep()
